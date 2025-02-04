4ae03c0ddcb78bb86b019a435989b7cff830b8b7
altanlabs <interfaces@altan.ai>
2025-02-04 09:14:16 +0000

    New commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom"
 import { ThemeProvider } from "@/theme/theme-provider"
+import { DatabaseProvider } from "@altanlabs/database"
 import Layout from "./layout"
 import { Toaster } from "@/components/ui/sonner"
 import { Home, ShoppingBag, MessageCircle, Heart, User } from "lucide-react"
@@ -58,18 +59,32 @@ const footerConfig = {
   ],
 }
 
+const databaseConfig = {
+  API_BASE_URL: "https://api.altan.ai/galaxia/hook/VnqJkO",
+  SAMPLE_TABLES: {
+    Products: "9b489e31-c7db-4515-924f-3c657d9d8eeb",
+    Users: "de4322c6-c229-4617-aef1-751086d85da2",
+    Categories: "c13a531a-56f8-4964-9e72-2893a6fae08e",
+    Transactions: "b6aa0add-3214-41c1-b1b0-c30553153d77",
+    Reviews: "3ee816b2-deee-4f5f-b805-d6b6475a8469",
+    Messages: "05342bb5-e386-4294-acef-db7400119db1"
+  }
+}
+
 function App() {
   return (
-    <ThemeProvider defaultTheme="light" storageKey="golf-exchange-theme">
-      <Router>
-        <Layout
-          sidebarConfig={sidebarConfig}
-          header={headerConfig}
-          footer={footerConfig}
-        />
-        <Toaster />
-      </Router>
-    </ThemeProvider>
+    <DatabaseProvider config={databaseConfig}>
+      <ThemeProvider defaultTheme="light" storageKey="golf-exchange-theme">
+        <Router>
+          <Layout
+            sidebarConfig={sidebarConfig}
+            header={headerConfig}
+            footer={footerConfig}
+          />
+          <Toaster />
+        </Router>
+      </ThemeProvider>
+    </DatabaseProvider>
   )
 }
 
