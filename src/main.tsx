e73e6ec9d3ecc788fe5025f3c96bb8fbcc1b5ed4
altanlabs <interfaces@altan.ai>
2025-02-04 09:15:22 +0000

    New commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import './App.css'
-import App from './App'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import App from "./App"
+import "./index.css"
+import IndexPage from "./pages/index"
+import NotFound from "./pages/NotFound"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <IndexPage />,
+      },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
